eae8bd2331e01cea7186472e9e01d1dfb28299b1
Onur Evren <onur.evren.1999@gmail.com>
2023-01-11 17:57:11 +0300

    Update

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<router-view></router-view>
+</template>
+
+<script>
+
+import {onBeforeMount} from "vue";
+
+import {loadScript} from "vue-plugin-load-script";
+
+export default {
+    name: "App",
+
+};
+
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/pages/auth/Login.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+  name: "Login"
+
+}
+</script>
+
+<style scoped>
+
+</style>
