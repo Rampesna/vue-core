50542c2b1da895a03edfc48120fd8a6ba57133e5
Onur Evren <onur.evren.1999@gmail.com>
2023-01-24 15:06:59 +0300

    Update Sidebar Menu

--- src/core/config/MainMenuConfig.ts
+++ src/core/config/MainMenuConfig.ts
@@ -12,17 +12,12 @@ const MainMenuConfig: Array<MenuItem> = [
   {
     pages: [
       {
-        heading: "dashboard",
+        heading: "Anasayfa",
         route: "/dashboard",
         svgIcon: "/media/icons/duotune/art/art002.svg",
         fontIcon: "bi-app-indicator",
       },
-      {
-        heading: "layoutBuilder",
-        route: "/builder",
-        svgIcon: "/media/icons/duotune/general/gen019.svg",
-        fontIcon: "bi-layers",
-      },
+
     ],
   },
   {
@@ -81,160 +76,7 @@ const MainMenuConfig: Array<MenuItem> = [
           },
         ],
       },
-      {
-        sectionTitle: "account",
-        route: "/account",
-        svgIcon: "/media/icons/duotune/communication/com006.svg",
-        fontIcon: "bi-person",
-        sub: [
-          {
-            heading: "accountOverview",
-            route: "/crafted/account/overview",
-          },
-          {
-            heading: "settings",
-            route: "/crafted/account/settings",
-          },
-        ],
-      },
-      {
-        sectionTitle: "authentication",
-        svgIcon: "/media/icons/duotune/technology/teh004.svg",
-        fontIcon: "bi-sticky",
-        sub: [
-          {
-            sectionTitle: "basicFlow",
-            sub: [
-              {
-                heading: "signIn",
-                route: "/sign-in",
-              },
-              {
-                heading: "signUp",
-                route: "/sign-up",
-              },
-              {
-                heading: "passwordReset",
-                route: "/password-reset",
-              },
-            ],
-          },
-          {
-            heading: "multiStepSignUp",
-            route: "/multi-step-sign-up",
-          },
-          {
-            heading: "error404",
-            route: "/404",
-          },
-          {
-            heading: "error500",
-            route: "/500",
-          },
-        ],
-      },
-      {
-        sectionTitle: "modals",
-        route: "/modals",
-        svgIcon: "/media/icons/duotune/art/art009.svg",
-        fontIcon: "bi-shield-check",
-        sub: [
-          {
-            sectionTitle: "general",
-            route: "/general",
-            sub: [
-              {
-                heading: "inviteFriends",
-                route: "/crafted/modals/general/invite-friends",
-              },
-              {
-                heading: "viewUsers",
-                route: "/crafted/modals/general/view-user",
-              },
-              {
-                heading: "upgradePlan",
-                route: "/crafted/modals/general/upgrade-plan",
-              },
-              {
-                heading: "shareAndEarn",
-                route: "/crafted/modals/general/share-and-earn",
-              },
-            ],
-          },
-          {
-            sectionTitle: "forms",
-            route: "/forms",
-            sub: [
-              {
-                heading: "newTarget",
-                route: "/crafted/modals/forms/new-target",
-              },
-              {
-                heading: "newCard",
-                route: "/crafted/modals/forms/new-card",
-              },
-              {
-                heading: "newAddress",
-                route: "/crafted/modals/forms/new-address",
-              },
-              {
-                heading: "createAPIKey",
-                route: "/crafted/modals/forms/create-api-key",
-              },
-            ],
-          },
-          {
-            sectionTitle: "wizards",
-            route: "/wizards",
-            sub: [
-              {
-                heading: "twoFactorAuth",
-                route: "/crafted/modals/wizards/two-factor-auth",
-              },
-              {
-                heading: "createApp",
-                route: "/crafted/modals/wizards/create-app",
-              },
-              {
-                heading: "createAccount",
-                route: "/crafted/modals/wizards/create-account",
-              },
-            ],
-          },
-        ],
-      },
-      {
-        sectionTitle: "widgets",
-        route: "/widgets",
-        svgIcon: "/media/icons/duotune/general/gen025.svg",
-        fontIcon: "bi-layers",
-        sub: [
-          {
-            heading: "widgetsLists",
-            route: "/crafted/widgets/lists",
-          },
-          {
-            heading: "widgetsStatistics",
-            route: "/crafted/widgets/statistics",
-          },
-          {
-            heading: "widgetsCharts",
-            route: "/crafted/widgets/charts",
-          },
-          {
-            heading: "widgetsMixed",
-            route: "/crafted/widgets/mixed",
-          },
-          {
-            heading: "widgetsTables",
-            route: "/crafted/widgets/tables",
-          },
-          {
-            heading: "widgetsFeeds",
-            route: "/crafted/widgets/feeds",
-          },
-        ],
-      },
+
     ],
   },
 
